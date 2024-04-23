f54f5e45df8bd7efd4974a185bb73bf03e3f1bc2
Vinicius Guerra <vini.fms@hotmail.com>
2024-04-23 16:21:23 -0300

    Add AppErrors and services Session init/track init

---
+++ src/services/track.services.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../database/database";
+import { Track, TrackPayloadCreate, TrackRetrieve } from "../interfaces/track.schemas";
+import { trackSchema, trackRetrieveSchema } from "../schemas";
+
+export class TrackService {
+  private track = prisma.track;
+
+  public list = async (): Promise<Array<Track>> => {
+    const tracks = await this.track.findMany();
+
+    return trackSchema.array().parse(tracks);
+  };
+
+  public create = async (payload: TrackPayloadCreate): Promise<Track> => {
+    const newTrack = await this.track.create({ data: payload });
+
+    return trackSchema.parse(newTrack);
+  };
+
+  public listByAlbumId = async (albumId: number): Promise<Array<Track>> => {
+    const albumTracks = await this.track.findMany({
+      where: { albumId: albumId },
+    });
+
+    return trackSchema.array().parse(albumTracks);
+  };
+
+  public retrieve = async (trackId: number): Promise<TrackRetrieve> => {
+    const track = await this.track.findUnique({
+      where: { id: trackId },
+      include: { album: true },
+    });
+
+    return trackRetrieveSchema.parse(track);
+  };
+}
