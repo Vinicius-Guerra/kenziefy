27b76aa33ccab4f8338ac4f83fc32c6d12401b63
Vinicius Guerra <vini.fms@hotmail.com>
2024-04-23 16:41:29 -0300

    implements services and add index

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export { BandServices, bandService } from "./band.services";
+export { AlbumService } from "./album.services";
+export { TrackService } from "./track.services";
+export { MusicianService } from "./musician.services";
+export { AccountService } from "./account.services";
+export { SessionServices } from "./session.services";
+export { BandInMemoryService } from "./bandInMemory.services";
---
+++ src/services/session.services.ts
@@ -0,0 +1,46 @@
+import { compare } from "bcrypt";
+import { prisma } from "../database/database";
+import { AppError } from "../errors/appError";
+import { SessionBodyCreate } from "../interfaces/session.interfaces";
+import { sessionBodyCreateSchema } from "../schemas";
+import { status } from "../utils/httpStatusCode";
+import { jwtConfig } from "../configs";
+import { sign } from "jsonwebtoken";
+
+export class SessionServices {
+    private account = prisma.account;
+
+    public login = async ({
+        username,
+        password,
+      }: SessionBodyCreate): Promise<SessionReturn> => {
+        // VERIFY USERNAME
+        const foundUser = await this.account.findFirst({
+          where: { username: username },
+        });
+
+        if(!foundUser) {
+            throw new AppError("Invalid credentials.", status.HTTP_401_UNAUTHORIZED);
+        }
+   
+        // VERIFY PASSWORD
+        const passwordMatch = await compare(password, foundUser.password);
+        if(!passwordMatch) {
+            throw new AppError("Invalid credentials.", status.HTTP_401_UNAUTHORIZED);
+        }
+
+        const { secret, expiresIn } = jwtConfig();
+
+        // GENERATOR JWT
+        const token = sign(
+            { favoriteColor: foundUser.favoriteColor, role: foundUser.role },
+            secret,
+            {
+                expiresIn: expiresIn,
+                subject: foundUser.id.toString(),
+            }
+        );
+
+        return sessionReturnSchema.parse({ token });
+    }
+}
