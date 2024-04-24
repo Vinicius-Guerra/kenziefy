19815d7321b4ce18f332dbd11f9a2e6ac2b922b2
Vinicius Guerra <vini.fms@hotmail.com>
2024-04-24 16:44:46 -0300

    index router

---
+++ src/routers/index.ts
@@ -0,0 +1,5 @@
+export { bandRouter } from "./band.routers";
+export { albumRouter } from "./album.routers";
+export { trackRouter } from "./track.routers";
+export { accountRouter } from "./account.routers";
+export { sessionRouter } from "./session.routers";
