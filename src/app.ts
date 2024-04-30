7714791f9b7d6d7ebaac105a0e32c0c25c17cc3a
Vinicius Guerra <vini.fms@hotmail.com>
2024-04-30 09:30:10 -0300

    modified app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import "reflect-metadata";
-import "helmet";
-import "dotenv/config";
-import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
+
 import { handleErrors } from "./middlewares";
-import helmet from "helmet";
 import { initRoutes } from "./routers";
 import { jwtConfig } from "./configs";
 import { initSwagger } from "./configs/swagger";
@@ -14,16 +12,11 @@ const app = express();
 
 export const initApp = () => {
   app.use(cors());
-  app.use(helmet());
   app.use(express.json());
   initRoutes(app);
   jwtConfig();
   initSwagger(app);
   app.use(handleErrors);
 }
-
-
-
-
 
 export default app;
