4b99cdebee853b615f981f24a71e1b7753842a18
Vinicius Guerra <vini.fms@hotmail.com>
2024-04-30 09:20:46 -0300

    build

--- dist/routers/track.routers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.trackRouter = void 0;
-const express_1 = require("express");
-const track_controllers_1 = require("../controllers/track.controllers");
-exports.trackRouter = (0, express_1.Router)();
-const trackController = new track_controllers_1.TrackControllers();
-exports.trackRouter.get("", trackController.list);
-exports.trackRouter.get("/:trackId", trackController.retrieve);
