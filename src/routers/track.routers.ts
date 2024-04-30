03c48b2e381ddac812a3f4dae8f76979c3e415c4
Vinicius Guerra <vini.fms@hotmail.com>
2024-04-30 11:23:07 -0300

    Route post track

--- src/routers/track.routers.ts
+++ src/routers/track.routers.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { TrackControllers } from "../controllers/track.controllers";
 import { ensure } from "../middlewares";
+import { trackBodyCreateSchema } from "../schemas";
 
 
 export const trackRouter = Router();
@@ -8,3 +9,4 @@ const trackController = new TrackControllers();
 
 trackRouter.get("", trackController.list);
 trackRouter.get("/:trackId", trackController.retrieve);
+trackRouter.post("", ensure.bodyIsValid(trackBodyCreateSchema), trackController.create)
